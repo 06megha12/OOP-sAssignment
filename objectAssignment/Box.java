65ba1172e33441245958ba0f9c17aacbfc388aab
06megha12 <112416048+06megha12@users.noreply.github.com>
2022-08-29 23:10:20 +0530

    Add files via upload

---
+++ objectAssignment/Box.java
@@ -0,0 +1,27 @@
+package com.objectAssignment;
+
+public class Box {
+
+	 int width;
+     int height;
+     int depth;
+     int volume;
+     public Box(int width, int height, int depth) {
+            super();
+            this.width = width;
+            this.height = height;
+            this.depth = depth;
+        }
+     
+     void volumeOfSphere()
+     {
+         volume=width*depth*height;
+         System.out.println(volume);
+         
+     }
+	public static void main(String[] args) {
+		  Box b=new Box(11,11,11);
+	         b.volumeOfSphere();
+	}
+
+}
---
+++ objectAssignment/Patient.java
@@ -0,0 +1,28 @@
+package com.objectAssignment;
+
+public class Patient {
+
+	String patienteName;
+	double height;
+	double weight;
+
+	public Patient(String patienteName, double height, double weight) {
+		super();
+		this.patienteName = patienteName;
+		this.height = height;
+		this.weight = weight;
+	}
+
+	double computeBMI() {
+
+		return weight % height * height;
+
+	}
+
+	public static void main(String[] args) {
+		Patient p = new Patient("Megha", 50, 60);
+		double computeBMI = p.computeBMI();
+		System.out.println(computeBMI);
+	}
+
+}
